35d4028f78aacc2dc5ac0061beb8e5dc902b450a
LucasVivas <lucas.vivas33@gmail.com>
2018-12-05 23:01:23 +0100

    create table at the begin of docker

--- src/postgresql/issues.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `playlist` (
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `description` TEXT NOT NULL
-
-    PRIMARY KEY (`id`)
-);
---
+++ src/postgresql/playlist.sql
@@ -0,0 +1,8 @@
+CREATE TABLE public.playlist
+(
+    id bigint NOT NULL,
+    PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+);
