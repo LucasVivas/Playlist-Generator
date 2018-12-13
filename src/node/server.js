f9913b895e44a5906a8f61285abcf758876468c5
LucasVivas <lucas.vivas33@gmail.com>
2018-12-13 17:52:01 +0100

    add skeleton of node function

--- src/node/server.js
+++ src/node/server.js
@@ -28,21 +28,68 @@ client.query('SELECT $1::text as message', ['Connection successful with postgres
 app.use(cors());
 app.use(morgan('combined'));
 
-app.get('/', function(req, res) {
+app.get('/', async function(req, res) {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.send('bonsoir toi');
 });
 
-app.get('/users', function(req, res) {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.send('nothing');
+/*
+ * /playlist
+ */
+
+app.post('/playlist', async function(req, res) {
+
 });
 
-app.get('/user/:firstname/:lastname', function(req, res) {
-    res.send('Bonjour utilisateur ' + req.params.firstname + ' ' + req.params.lastname);
+/*
+ * /playlist/:playlist_id
+ */
+
+app.get('/playlist/:playlist_id', async function(req, res) {
+
 });
+
+app.post('/playlist/:playlist_id', async function(req, res) {
+
+});
+
+app.put('/playlist/:playlist_id', async function(req, res) {
+
+});
+
+app.delete('/playlist/:playlist_id', async function(req, res) {
+
+});
+
+/*
+ * /playlist/:playlist_id/:track_id
+ */
+
+app.get('/playlist/:playlist_id/:track_id', async function(req, res) {
+
+});
+
+app.put('/playlist/:playlist_id/:track_id', async function(req, res) {
+
+});
+
+app.delete('/playlist/:playlist_id/:track_id', async function(req, res) {
+
+});
+
+/*
+ * /playlists
+ */
+
+app.get('/playlists', async function(req, res) {
+
+});
+
+app.delete('/playlists', async function(req, res) {
+
+});
+
 
 app.use(function(req, res, next) {
     res.status(404).send('Page introuvable !');
