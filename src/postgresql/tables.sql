b44e68df3011e5a0687d0532b7a7ffb839a69e1f
LucasVivas <lucas.vivas33@gmail.com>
2018-12-24 13:01:17 +0100

    tables made and add some stuff in utils

--- src/postgresql/tables.sql
+++ src/postgresql/tables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE public.playlist
+CREATE TABLE public."Playlists"
 (
     id integer NOT NULL,
     name character[] NOT NULL,
@@ -12,10 +12,24 @@ WITH (
 CREATE TABLE public."Tracks"
 (
     id integer NOT NULL,
-    spotify_id character[] NOT NULL,
+    name character[] NOT NULL,
+    artist character[] NOT NULL,
+    album character[] ,
     PRIMARY KEY (id)
 )
 WITH (
     OIDS = FALSE
 );
-      
+
+CREATE TABLE public."Playlist_tracks"
+(
+    id integer NOT NULL,
+    playlist_id integer,
+    track_id integer,
+    PRIMARY KEY (id),
+    FOREIGN KEY (playlist_id) REFERENCES public."Playlists" (id),
+    FOREIGN KEY (track_id) REFERENCES public."Tracks" (id)
+)
+WITH (
+    OIDS = FALSE
+);
