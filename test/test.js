84b45f6580210e90138004a5b14c9f2ebb072acf
LucasVivas <lucas.vivas33@gmail.com>
2018-12-29 19:28:25 +0100

    add test mocha

---
+++ test/test.js
@@ -0,0 +1,61 @@
+const assert = require('assert');
+
+describe('GET /playlist/{playlist_id}', () => {
+  it('should ', () => {
+    assert.equal(1 + 1, 2);
+  });
+});
+
+describe('POST /playlist/{playlist_id}', () => {
+  it('should ', () => {
+    assert.equal(1 + 1, 2);
+  });
+});
+
+describe('PUT /playlist/{playlist_id}', () => {
+  it('should ', () => {
+    assert.equal(1 + 1, 2);
+  });
+});
+
+describe('DELETE /playlist/{playlist_id}', () => {
+  it('should ', () => {
+    assert.equal(1 + 1, 2);
+  });
+});
+
+describe('POST /playlist', () => {
+  it('should ', () => {
+    assert.equal(1 + 1, 2);
+  });
+});
+
+describe('GET /playlist/{playlist_id}/{track_id}', () => {
+  it('should ', () => {
+    assert.equal(1 + 1, 2);
+  });
+});
+
+describe('PUT /playlist/{playlist_id}/{track_id}', () => {
+  it('should ', () => {
+    assert.equal(1 + 1, 2);
+  });
+});
+
+describe('DELETE /playlist/{playlist_id}/{track_id}', () => {
+  it('should ', () => {
+    assert.equal(1 + 1, 2);
+  });
+});
+
+describe('GET /playlists', () => {
+  it('should ', () => {
+    assert.equal(1 + 1, 2);
+  });
+});
+
+describe('DELETE /playlists', () => {
+  it('should ', () => {
+    assert.equal(1 + 1, 2);
+  });
+});
