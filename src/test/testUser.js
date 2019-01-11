79b5bca2eede8c8f59aaa64daa5c35daa3711cb3
LucasVivas <lucas.vivas33@gmail.com>
2019-01-11 13:03:46 +0100

    test user finished

---
+++ src/test/testUser.js
@@ -0,0 +1,185 @@
+process.env.NODE_ENV = 'test';
+
+const assert = require('assert');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const expect = chai.expect;
+
+const server = 'http://localhost:8080';
+
+const user1 = { username: 'myUsername1', password: 'myPassword1' };
+const user2 = { username: 'myUsername2', password: 'myPassword2' };
+const playlist = { name: 'myPlaylist', description: 'myDescription' };
+
+const urlMultiple = '/users';
+const urlSingle = '/user';
+const urlUserIdParam = '/user_id';
+
+chai.use(chaiHttp);
+
+function testAsync(done, fn) {
+  try {
+    fn();
+    done();
+  } catch (err) {
+    done(err);
+  }
+}
+
+describe('User', () => {
+  beforeEach((done) => {
+    chai.request(server)
+      .delete(urlMultiple)
+      .end(() => {
+        done();
+      });
+  });
+
+  describe(`GET ${urlMultiple}`, () => {
+    it('Should get an Array with 2 user (Code: 200)', (done) => {
+      chai.request(server)
+        .post(urlSingle)
+        .send(user1);
+      chai.request(server)
+        .post(urlSingle)
+        .send(user2);
+      chai.request(server)
+        .get(urlMultiple)
+        .end((err, res) => {
+          testAsync(done, (() => {
+            expect(res).to.have.status(200);
+            expect(res.body).to.deep.equal([]);
+          }));
+        });
+    });
+  });
+
+  describe(`DELETE ${urlMultiple}`, () => {
+    it('Should delete the db (Code: 200)', (done) => {
+      chai.request(server)
+        .delete(urlMultiple)
+        .end((err, res) => {
+          testAsync(done, (() => {
+            expect(res).to.have.status(200);
+          }));
+        });
+    });
+  });
+
+  describe(`GET ${urlSingle}${urlUserIdParam}`, () => {
+    it('Should get the user (Code: 200)', (done) => {
+      chai.request(server)
+        .post(urlSingle)
+        .send(user1);
+      chai.request(server)
+        .get(`${urlSingle}/1`)
+        .end((err, res) => {
+          testAsync(done, (() => {
+            expect(res).to.have.status(200);
+            expect(res.body).to.deep.equals(user1);
+          }));
+        });
+    });
+    it('Should not found any user (Code: 404)', (done) => {
+      chai.request(server)
+        .get(`${urlSingle}/2`)
+        .end((err, res) => {
+          testAsync(done, (() => {
+            expect(res).to.have.status(404);
+          }));
+        });
+    });
+  });
+
+  describe(`POST ${urlSingle}`, () => {
+    it('Should add an user (Code: 201)', (done) => {
+      chai.request(server)
+        .post(urlSingle)
+        .send(user1)
+        .end((err, res) => {
+          testAsync(done, (() => {
+            expect(res).to.have.status(201);
+          }));
+        });
+    });
+    it('Should cannot add a user because already exist (Code: 409)', (done) => {
+      chai.request(server)
+        .post(urlSingle)
+        .send(user1);
+      chai.request(server)
+        .post(urlSingle)
+        .send(user1)
+        .end((err, res) => {
+          testAsync(done, (() => {
+            expect(res).to.have.status(409);
+          }));
+        });
+    });
+  });
+
+  describe(`PUT ${urlSingle}${urlUserIdParam}`, () => {
+    it('Should modify an user (Code: 200)', (done) => {
+      chai.request(server)
+        .post(urlSingle)
+        .send(user1);
+      chai.request(server)
+        .put(`${urlSingle}/1`)
+        .send(user2)
+        .end((err, res) => {
+          testAsync(done, (() => {
+            expect(res).to.have.status(200);
+          }));
+        });
+    });
+    it('Should modify an undefined user (Code: 404)', (done) => {
+      chai.request(server)
+        .put(`${urlSingle}/1`)
+        .end((err, res) => {
+          testAsync(done, (() => {
+            expect(res).to.have.status(404);
+          }));
+        });
+    });
+    it('Should cannot modify an user into another who already exist (Code: 409)', (done) => {
+      chai.request(server)
+        .post(urlSingle)
+        .send(user1);
+      chai.request(server)
+        .post(urlSingle)
+        .send(user2);
+      chai.request(server)
+        .put(`${urlSingle}/1`)
+        .send(user2)
+        .end((err, res) => {
+          testAsync(done, (() => {
+            expect(res).to.have.status(409);
+          }));
+        });
+    });
+  });
+
+  describe(`DELETE ${urlSingle}${urlUserIdParam}`, () => {
+    it('Should delete an user (Code: 200)', (done) => {
+      chai.request(server)
+        .post(urlSingle)
+        .send(user1);
+      chai.request(server)
+        .delete(`${urlSingle}/1`)
+        .end((err, res) => {
+          testAsync(done, (() => {
+            expect(res).to.have.status(200);
+          }));
+        });
+    });
+    it('Should cannot add a user because already exist (Code: 404)', (done) => {
+      chai.request(server)
+        .delete(`${urlSingle}/1`)
+        .end((err, res) => {
+          testAsync(done, (() => {
+            expect(res).to.have.status(404);
+          }));
+        });
+    });
+  });
+});
